ca16504791dfe6c896a74b39d732c3bbcc3a6200
Stuart Parkington <narrf@narrf.net>
2019-06-14 10:26:42 +0100

    Added .gitignore, CspLuiMonthlyReporting.ps1 and updated README.md

---
+++ CspLuiMonthlyReporting.ps1
@@ -0,0 +1,227 @@
+###############################################
+#       CspLuiMonthlyReporting.ps1
+#
+# This script is intended to utilise the Citrix Cloud licensing
+# API to display the reported LUI license usage, for a given partner,
+# for a given month.
+#
+# https://developer.cloud.com/index.html
+# https://licensing.citrixworkspacesapi.net/Help
+#
+# USE AT OWN RISK - NO WARANTY PROVIDED
+#
+# Author. Stuart Parkington, Lead SE, CSP EMEA
+#
+
+# Accept command line parameters of -Year and/or -Month
+# If none are supplied, defaults to the previous month.
+# The parameter -Detail will provide reporting per server
+# per month. 
+param (
+  [int]$year = (Get-Date).year,                     # Set default year as current Year
+  [int]$month = (Get-Date).month-1,                 # Set default month to current month - 1
+  [switch]$detail = $false,                         # Switch to toggle detail report
+  [switch]$csv = $false,                            # Switch to toggle CSV file output
+  [string]$csvfilepath = "./",                      # Set default path for CSV export to current working directory
+  [switch]$quiet = $false,                           # Switch to toggle screen output
+  [switch]$send = $false
+)
+
+# Specify Citrix Cloud API credentials 
+$clientId = "93aea4f8-968d-4cc3-8514-d309e0493730"  #Replace with your clientId
+$clientSecret = "mOqSQIvqUiCTVzuJyJykBw=="          #Replace with your clientSecret
+$customerId = "BuckeyeCSP81"                        #Replace with your customer name
+
+# Correct month and date, if previous month is in previous year (e.g. January)
+if ( $month -eq 0 ) {
+    $month = 12
+    $year = $year - 1
+}
+
+# Variables - do not change
+$firstcall = $true
+$csvfilepath = $csvfilepath + "LUI_Report_" + $customerId + "_" + $year + "-" + $month + ".csv"
+
+
+# Create date string for API call, padding month with 0's if required 
+[string]$reportdate = $year.ToString() + "-" + $month.ToString("00")
+
+
+# Grab API Bearer Token
+function GetBearerToken {
+  param (
+    [Parameter(Mandatory=$true)]
+    [string] $clientId,
+    [Parameter(Mandatory=$true)]
+    [string] $clientSecret
+  )
+
+  $postHeaders = @{"Content-Type"="application/json"}
+  $body = @{
+    "ClientId"=$clientId;
+    "ClientSecret"=$clientSecret
+  }
+
+  $trustUrl = "https://trust.citrixworkspacesapi.net/root/tokens/clients"
+
+  try {
+    $response = Invoke-RestMethod -Uri $trustUrl -Method POST -Body (ConvertTo-Json $body) -Headers $postHeaders
+  } catch {
+    # Dig into the exception to get the Response details.
+    # Note that value__ is not a typo.
+    write-host "Error making REST request to:`n" $restUrl -ForegroundColor Red
+    Write-Host "`nStatus Code:" $_.Exception.Response.StatusCode.value__ -ForegroundColor Red
+    Write-Host "Status Description:" $_.Exception.Response.StatusDescription -ForegroundColor Red
+    exit
+  }
+
+  $bearerToken = $response.token
+
+  return $bearerToken;
+}
+
+# Retrieve all License Usage Data for given month. Usage returned as JSON
+function GetCSPLicenseUsageData {
+  param (
+    [Parameter(Mandatory=$true)]
+    [string] $customerId,
+    [Parameter(Mandatory=$true)]
+    [string] $reportdate
+  )
+
+  $postHeaders = @{
+    "Content-Type"="application/json";
+    "Authorization"="CwsAuth bearer=$($bearerToken)"
+  }
+
+  $Url = "https://licensing.citrixworkspacesapi.net/$($customerId)/licenseusagedata?date=$($reportdate)"
+
+  try {
+    $licenseUsage = Invoke-RestMethod -Uri $Url -Method GET -Headers $postHeaders
+  } catch {
+    # Dig into the exception to get the Response details.
+    # Note that value__ is not a typo.
+    write-host "Error making REST request to:`n" $restUrl -ForegroundColor Red
+    Write-Host "`nStatus Code:" $_.Exception.Response.StatusCode.value__ -ForegroundColor Red
+    Write-Host "Status Description:" $_.Exception.Response.StatusDescription -ForegroundColor Red
+    exit
+  }
+
+  if ( $licenseUsage.skus.skuId -eq $null ) {
+    write-host "No user information available for the selected month ($($reportdate))." -ForegroundColor White -BackgroundColor Red
+    exit
+  }
+
+  return $licenseUsage
+}
+
+# Extract simple useage counts for each SKU specified, by featureID (e.g. XDT_PLT_UD)
+# Returns an object
+function GetUsagePerSku {
+  Param (
+    [Parameter(Mandatory=$true)]
+    [string] $featureID
+  )
+
+  $sku = $usage.skus.features | Where-Object {$_.'featureID' -eq $featureID}
+
+  if ( [int]$sku."count" -le 0 ) {
+    $freePercent = "0"
+  } else {
+    $freePercent = [int](([int]$sku."freeUsersCount"/[int]$sku."count") * 100)
+  }
+
+  $retValue = @(
+        [pscustomobject]@{
+          edition = $sku.featureDescription                           # featureDescription (e.g. Premium/Base/VDI)
+          totalUsers =$sku.count                                      # Total user count
+          paidUsers = [int]$sku.count - [int]$sku.freeUsersCount      # Paid user count (e.g. total - free)
+          freeUsers = $sku.freeUsersCount                             # Free user count
+          freeUserPercent = $freePercent                              # Percentage of free users claimed (e.g. free/total * 100
+        }
+    )   
+
+  return $retValue
+}
+
+
+# Produce pretty display to console, with pretty colours and/or create .csv file! :)
+function OutputUsagePerSku {
+  Param (
+    [Parameter(Mandatory=$true)]
+    [string] $featureID
+  )
+
+  $skuUsage = GetUsagePerSku $featureID
+
+  if ( $skuUsage.edition -eq "Premium" ) {
+    $skuUsage.edition = "Citrix Virtual Apps and Desktops, Premium"
+  } elseif ( $skuUsage.edition -eq "Base" ) {
+    $skuUsage.edition = "Citrix Virtual Apps, Standard"
+  } elseif ( $skuUsage.edition -eq "VDI Edition" ) {
+    $skuUsage.edition = "Citrix Virtual Desktops, Standard"
+  } 
+
+  if ( $quiet -eq $false ) {
+    if ( $firstcall -eq $true ) {
+      write-host "`nCSP LUI Usage for" $customerId "for" (Get-Culture).DateTimeFormat.GetMonthName($month) $year
+      $script:firstcall = $false
+    }
+
+    write-host $skuUsage.edition -ForegroundColor Yellow -NoNewLine
+    Write-Host "".PadRight((50-$skuUsage.edition.length)," ") -NoNewLine
+    write-host "Total: " -NoNewLine
+    write-host $skuUsage.totalUsers -ForegroundColor Yellow  -NoNewLine
+    Write-Host "".PadRight((6-$skuUsage.totalUsers.ToString().length)," ") -NoNewLine
+    write-host "Paid: " -NoNewLine
+    write-host $skuUsage.paidUsers -ForegroundColor Red -NoNewLine
+    Write-Host "".PadRight((6-$skuUsage.paidUsers.ToString().length)," ") -NoNewLine
+    write-host "Free: " -NoNewLine
+    write-host $skuUsage.freeUsers -ForegroundColor Green -NoNewLine
+    write-host " ($($skuUsage.freeUserPercent)%)" -ForegroundColor Gray
+
+    # Display SKU usage per Server, if -detail flag set, showing servers marked as free in green.
+    if ( $detail -eq $true ) {
+      $serverSkuUsage = $usage.skus.features | Where-Object {$_.'featureID' -eq $featureID}
+      foreach ( $server in $serverSkuUsage.licenseServerUsages ) {
+        $foreColor = "DarkGray"
+        if ( $server.isLicenseServerFree -eq $true ) {
+          $foreColor = "Green"
+        }
+        Write-Host "".PadLeft(8," ") $server.fqdn "".PadRight(39-$server.fqdn.length, " ") `
+                   "total:" $server.count "".PadRight((4-$server.count.tostring().length), " ") `
+                   "paid:" ([int]$server.count - [int]$server.freeUsersCount) "".PadRight(4-([int]$server.count - [int]$server.freeUsersCount).ToString().length ," ") `
+                   "free:" $server.freeUsersCount -ForegroundColor $foreColor
+      }
+    }
+  }
+
+  # Write CSV output file required at path specified by variable $csvfilepath
+  if ( $csv -eq $true) {
+    # $csvfilepath = $csvfilepath + "LUI_Report_" + $customerId + "_" + $year + "-" + $month + ".csv"
+    $csvusage = @(
+        [pscustomobject]@{
+          Edition = $skuUsage.edition
+          Total = $skuUsage.totalUsers
+          Paid = $skuUsage.paidUsers
+          Free = $skuUsage.freeUsers
+          PercentFree = $skuUsage.freeUserPercent
+        }
+      )
+    $csvusage | Export-Csv -Path $csvfilepath -Append -NoTypeInformation
+  }
+}
+
+## MAIN
+$bearerToken = GetBearerToken $clientId $clientSecret
+
+$usage = GetCSPLicenseUsageData $customerId $reportdate
+
+OutputUsagePerSku "XDT_PLT_UD"
+OutputUsagePerSku "XDT_ADV_UD" 
+OutputUsagePerSku "XDT_STD_UD" 
+
+if (( test-path $csvfilepath ) -and ($send -eq $true )) {
+  $file = $csvfilepath
+  Write-Output $file
+}
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-csp-lui-scripts
+# CSP LUI Scripts
+These scripts have been written to assist Citrix Service Providers utilise the [Citrix Cloud Licensing API](https://licensing.citrixworkspacesapi.net) to query usage and status. They are read-only and **do not** make any amendments, simply executes GET requests for data, with some options for dumping out the results, either to the console or .csv file (or both).
+
+**Use at your own risk** as these are not Citrix supported scripts. They have been written by myself as a proof-of-concept.
+
+Written with and **only tested on** PowerShell Core 6+ - they will error if used with earlier versions.
+
+### Citrix Cloud API Secure Client Token
+A Citrix Cloud API Secure Client token will need to be created and the variables at the start of each script amended.
+ 
+To create an API key:
+1.	Go to ‘hamburger’ menu in CC and select **Identity and Access Management**
+2.	Select the **API Access** tab
+3.	Note the **CustomerID** from the screen
+4.	Specify a name for the client and hit the **Create Client** button
+![CustomerID](https://www.dropbox.com/s/7sps1ncf3wzrce2/cc_api_customerid.png?dl=0)
+5.	Download the resulting ID and Secret
+6.	Replace the values for **`$clientId`**, **`$clientSecret`** and **`$customerID`** with the values you noted/downloaded, towards the top of the script, in the section marked **`# Specify Citrix Cloud API credentials`**
+
+### CspLuiMonthlyReporting.ps1
+Ran without any switches, **CspLuiMonthlyReporting.ps1** will default to querying the usage for the previous month, displaying totals for each SKU, paid and free users. The following switches are available:
+ 
+* **`-month xx`** (where xx is the month number required) specifies the month to query
+* **`-year xxxx`** (where xxxx is the year) specifies the year to query
+* **`-detail`** forces the output (not CSV output) to show detail per server (as well as per SKU)
+* **`-csv`** causes a comma delimited file of the SKU usage to be dumped to the current directory. If a different directory is required, amend the default value at the top of the script or use the -csvfilepath switch. The file created will be named **`LUI_Report_<customerID>_<year>-<month>.csv`**.
+* **`-csvfilepath`** amends the file output path for the .csv file (e.g. -csvfilepath c:\temp). Only relevant with the -csv switch above.
+* **`-quiet`** prevents output to screen
+* **`-send`** causes the .csv file name to be output to the PoSH pipeline
+
